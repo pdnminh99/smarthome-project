8eb7483ff2e2264ff908e4e1f81f1bd6aeeb22f3
nhatminh <pdn.minh1999@gmail.com>
2019-11-15 19:14:40 +0700

    create prototype of module view and home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { IconsProviderModule } from './icons-provider.module';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { ChartsComponent } from './components/charts/charts.component';
-import { environment } from 'src/environments/environment';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireModule } from '@angular/fire';
-import { ChartsModule } from 'ng2-charts';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { ModuleViewComponent } from './components/module-view/module-view.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { IconsProviderModule } from "./icons-provider.module";
+import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { ChartsComponent } from "./components/charts/charts.component";
+import { environment } from "src/environments/environment";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireModule } from "@angular/fire";
+import { ChartsModule } from "ng2-charts";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { ModuleViewComponent } from "./components/module-view/module-view.component";
+import { HomeViewComponent } from './components/home-view/home-view.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChartsComponent,
-    ModuleViewComponent,
-  ],
+  declarations: [AppComponent, ChartsComponent, ModuleViewComponent, HomeViewComponent],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
--- src/app/components/module-view/module-view.component.ts
+++ src/app/components/module-view/module-view.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-module-view',
-  templateUrl: './module-view.component.html',
-  styleUrls: ['./module-view.component.scss']
+  selector: "app-module-view",
+  templateUrl: "./module-view.component.html",
+  styleUrls: ["./module-view.component.scss"]
 })
 export class ModuleViewComponent implements OnInit {
+  panels = [
+    {
+      active: true,
+      disabled: false,
+      name: "Living room",
+      childPannel: [
+        {
+          active: false,
+          disabled: true,
+          name: "This is panel header 1-1"
+        }
+      ]
+    },
+    {
+      active: false,
+      disabled: true,
+      name: "This is panel header 2"
+    },
+    {
+      active: false,
+      disabled: false,
+      name: "This is panel header 3"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
